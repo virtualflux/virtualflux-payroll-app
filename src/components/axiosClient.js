869977b58679f88600a6553557cd4374c1ae4097
virtualflux <info@virtualflux.africa>
2025-10-14 11:16:52 +0100

    feat: login api

---
+++ src/components/axiosClient.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { store } from "@/state/store";
+import { logout, loginSuccess } from "@/state/slices/user.slice";
+
+const axiosClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+});
+
+axiosClient.interceptors.request.use((config) => {
+  const state = store.getState();
+  const token = state.user.accessToken;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+// axiosClient.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
+//     if (error.response?.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
+//       try {
+//         const { data } = await axios.post("/api/auth/refresh");
+//         store.dispatch(loginSuccess(data));
+//         originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
+//         return axiosClient(originalRequest);
+//       } catch (err) {
+//         store.dispatch(logout());
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+export default axiosClient;
